8943dca0d086c5ff2e8c226c8abef6dece631305
Gabriel Carmelo Villaverde <vgabrielcv@gmail.com>
2023-08-18 20:23:51 -0500

    Added header to tweet page

--- src/app/tweet/[id]/page.tsx
+++ src/app/tweet/[id]/page.tsx
@@ -6,12 +6,13 @@ import { getTweetById } from '@/lib/supabase/queries'
 import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
 import dayjs from 'dayjs'
 import { cookies } from 'next/headers'
+import Link from 'next/link'
 import { redirect } from 'next/navigation'
 import router from 'next/router'
 import React from 'react'
 import { AiOutlineRetweet, AiFillHeart, AiOutlineHeart } from 'react-icons/ai'
 import { BsDot, BsThreeDots } from 'react-icons/bs'
-import { IoStatsChart, IoShareOutline } from 'react-icons/io5'
+import { IoStatsChart, IoShareOutline, IoArrowBackOutline } from 'react-icons/io5'
 
 type TweetReplyReturnType = Database['public']['Tables']['replies']['Row'] & {
     profiles: Pick<
@@ -42,6 +43,14 @@ const TweetPage = async ({ params }: { params: { id: string } }) => {
         <main className="flex xl:w-[50%] h-full min-h-screen flex-col
           border-l-[0.5px] border-r-[0.5px] border-gray-600"
         >
+            <div className="text-xl font-bold my-4 p-6 backdrop-blur bg-black/10 sticky top-0 border-b-[0.5px] border-gray-600 flex flex-row items-center gap-x-3">
+                <div className="rounded-full hover:bg-white/10 transition duration-200 cursor-pointer p-2">
+                    <Link href="/">
+                        <IoArrowBackOutline />
+                    </Link>
+                </div>
+                <h1>Post</h1>
+            </div>
             {
                 tweet ? <Tweet tweet={tweet} />
                     : <div>No tweet found</div>
